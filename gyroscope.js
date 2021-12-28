7c86a686cd3b39d670c284c377cc03f57446516f
BayAji <bayuajiwicaksono64@gmail.com>
2021-12-28 10:09:12 +0700

    Update gyroscope.js
    update

--- gyroscope.js
+++ gyroscope.js
@@ -7,37 +7,7 @@
  * @license MIT License
  * @options frequency, callback
  */
- function onClick() {
-    // feature detect
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-      DeviceMotionEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('devicemotion', () => {});
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-    }
- function onClick() {
-    // feature detect
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('deviceorientation', () => {});
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-    }
-  }
-
-
-
-  }
+ 
  (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
             // AMD. Register as an anonymous module.
